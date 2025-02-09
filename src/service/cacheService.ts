c3c4b05551bfbe969e3583296ae9567ec0ad4fe2
eshaansinghparihar <eshaan.singh.parihar@gmail.com>
2025-02-09 13:26:43 +0530

    Added logic for updating cache

--- src/service/cacheService.ts
+++ src/service/cacheService.ts
@@ -3,15 +3,22 @@ import { CacheItem, ICacheResponse } from "../models/cacheItem";
 
 export class CacheService{
     async setCache(key: string, value: any, cache : Map<string, CacheItem>, isCacheFull : boolean) : Promise<ICacheResponse|undefined>{
-            if(!isCacheFull){
-                const timestamp = new Date().getTime()
-                const cacheItem  : CacheItem = { key, value, timestamp}
-                cache.set(key, cacheItem)
-                return {...cacheItem, message : 'Set Cache Successfully'};
-            }
-            else{
-                throw new CacheError('Cache Already Full - Cache Hit Maximum Size', 403);
-            }
+        
+        const timestamp = new Date().getTime();
+        const cacheItem: CacheItem = { key, value, timestamp };
+
+        if (cache.has(key)) {
+            cache.set(key, cacheItem);
+            return {...cacheItem, message: 'Updated Cache Successfully'};
+        }
+
+        if(!isCacheFull){
+            cache.set(key, cacheItem)
+            return {...cacheItem, message : 'Set Cache Successfully'};
+        }
+        else{
+            throw new CacheError('Cache Already Full - Cache Hit Maximum Size', 403);
+        }
     }
 
     async getCache(key: string, cache : Map<string, CacheItem>) : Promise<ICacheResponse|undefined> {
